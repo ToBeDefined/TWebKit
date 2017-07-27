962f09499024e7f041f8e7ebe1f29dccc3f207a4
邵伟男 <weinanshao@163.com>
2017-07-27 18:06:22 +0800

    修改项目为framework

---
+++ TWebKit/TWebKit.h
@@ -0,0 +1,25 @@
+//
+//  TWebKit.h
+//  TWebKit
+//
+//  Created by 邵伟男 on 2017/7/27.
+//  Copyright © 2017年 邵伟男. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TWebKit.
+FOUNDATION_EXPORT double TWebKitVersionNumber;
+
+//! Project version string for TWebKit.
+FOUNDATION_EXPORT const unsigned char TWebKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TWebKit/PublicHeader.h>
+
+
+#import "TWebView.h"
+#import "TWebViewConfig.h"
+#import "TWebViewController.h"
+#import "TWebViewCommonDelegate.h"
+
+
